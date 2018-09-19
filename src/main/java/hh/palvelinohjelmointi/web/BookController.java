cab2ea3370ba0d38ba3921418fb40ef08907b6c1
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2018-09-19 18:48:20 +0300

    Add editing functionality

--- src/main/java/hh/palvelinohjelmointi/web/BookController.java
+++ src/main/java/hh/palvelinohjelmointi/web/BookController.java
@@ -28,11 +28,17 @@ public class BookController {
     }     
     
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Book book){
+    public String saveBook(Book book){
         repository.save(book);
         return "redirect:booklist";
-    }    
-
+    }
+    
+	@RequestMapping (value="/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId,Model model){
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
+	
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
