59b8425469be690afa7197cc06e7f25e8d2139e3
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2018-10-03 20:19:02 +0300

    Add things

--- src/main/java/hh/palvelinohjelmointi/domain/CategoryRepository.java
+++ src/main/java/hh/palvelinohjelmointi/domain/CategoryRepository.java
@@ -3,7 +3,8 @@ package hh.palvelinohjelmointi.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     List<Category> findByName(String name);
--- src/main/java/hh/palvelinohjelmointi/web/BookController.java
+++ src/main/java/hh/palvelinohjelmointi/web/BookController.java
@@ -1,16 +1,20 @@
 package hh.palvelinohjelmointi.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.palvelinohjelmointi.domain.Book;
 import hh.palvelinohjelmointi.domain.BookRepository;
-import hh.palvelinohjelmointi.domain.Category;
 import hh.palvelinohjelmointi.domain.CategoryRepository;
+
 @Controller
 public class BookController {
 	@Autowired
@@ -38,6 +42,18 @@ public class BookController {
         return "redirect:booklist";
     }
     
+      // REST
+      @RequestMapping(value="/books", method = RequestMethod.GET)
+      public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+      // REST
+     @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+     public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }
+      
 	@RequestMapping (value="/edit/{id}")
 	public String editBook(@PathVariable("id") Long bookId,Model model){
 		model.addAttribute("book", repository.findById(bookId));
