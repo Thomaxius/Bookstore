9cbe8ccda63c260b40543ba3bbcc16760d19e757
Tommi <Tommi.Santamaa@gmail.com>
2018-10-16 23:21:33 +0300

    Added users, signing up, other things

--- src/main/java/hh/palvelinohjelmointi/BookstoreApplication.java
+++ src/main/java/hh/palvelinohjelmointi/BookstoreApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import hh.palvelinohjelmointi.domain.UserRepository;
+import hh.palvelinohjelmointi.domain.User;
 import hh.palvelinohjelmointi.domain.Book;
 import hh.palvelinohjelmointi.domain.BookRepository;
 import hh.palvelinohjelmointi.domain.Category;
@@ -16,13 +18,13 @@ import hh.palvelinohjelmointi.domain.CategoryRepository;
 @SpringBootApplication
 public class BookstoreApplication {
 	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) {
+	public CommandLineRunner bookDemo(BookRepository bookRepository, UserRepository urepository, CategoryRepository categoryRepository) {
 		return (args) -> {
 			log.info("Saving a couple of books and categories..");
 			categoryRepository.save(new Category("Kaunokirjallisuus"));
@@ -33,6 +35,12 @@ public class BookstoreApplication {
 					categoryRepository.findByName("Kaunokirjallisuus").get(0)));
 			bookRepository.save(new Book("Täällä Pohjantähden Alla", "Väinö Linna","159495-1", 1955, 
 					categoryRepository.findByName("Kaunokirjallisuus").get(0)));	
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "admin@bookstore.com");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@bookstore.com");
+			urepository.save(user1);
+			urepository.save(user2);
+			
 			
 			log.info("Fetching all books..");
 			for (Book book : bookRepository.findAll()) {
--- src/main/java/hh/palvelinohjelmointi/web/BookController.java
+++ src/main/java/hh/palvelinohjelmointi/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,13 +31,13 @@ public class BookController {
         return "login";
     }	
 	
-	
     @RequestMapping(value="/booklist")
     public String bookList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
   
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
@@ -68,6 +69,7 @@ public class BookController {
 		return "editbook";
 	}
 	
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
